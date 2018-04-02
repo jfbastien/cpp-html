b9105e9896caf68641c6ed7c210a4a85117a5521
dextercd <dexter.c.dopping@gmail.com>
2018-04-02 17:48:45 +0200

    add ol, ul, and il tags

--- hello.html.cpp
+++ hello.html.cpp
@@ -5,5 +5,15 @@
     ﹤／head﹥
     ﹤body﹥
         ﹤p﹥(Hello, World!)﹤／p﹥
+        ﹤ol﹥
+            ﹤li﹥(This is an item in an ordered list!)﹤／li﹥
+            ﹤li﹥(Pretty coolˎ huh?)﹤／li﹥
+            ﹤li﹥(This is the last item!)﹤／li﹥
+        ﹤／ol﹥
+
+        ﹤ul﹥
+            ﹤li﹥(This is an item in an unordered list!)﹤／li﹥
+            ﹤li﹥(Also cool!)﹤／li﹥
+        ﹤／ul﹥
     ﹤／body﹥
 ﹤／html﹥
